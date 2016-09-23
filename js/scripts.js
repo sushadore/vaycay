71e397025a2a1f28f2019593a4c8a12751b0c94c
Susha Dore <susha.dore@gmail.com>
2016-09-23 13:43:19 -0700

    add functionality to weather button regarding jaco and london

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+var jaco=0;
+var london=0;
+var newOrleans=0;
+var parkCity=0;
 
+$(document).ready(function() {
+ $("#temp button").click(function() {
+    var temperature = $("input:radio[name=weather]:checked").val();
+     if (temperature === "warm") {
+       return jaco += 1;
+     } else {
+       london += 1;
+     }
+
+   });
 });
