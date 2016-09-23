0428efc60230c37683c92dcb8b04bf0b56b216d9
Susha Dore <susha.dore@gmail.com>
2016-09-23 14:35:17 -0700

    create functioning radio button for temperature and passport options returning the correct vacation destination

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,30 @@ var newOrleans=0;
 var parkCity=0;
 
 $(document).ready(function() {
- $("#temp button").click(function() {
+ $("#result button").click(function() {
     var temperature = $("input:radio[name=weather]:checked").val();
-     if (temperature === "warm") {
-       return jaco += 1;
+    var passport = $("input:radio[name=passport]:checked").val();
+     if (temperature === "warm" && passport === "yes") {
+        $("#jaco").show();
+        $("#neworleans").hide();
+        $("#london").hide();
+        $("#parkcity").hide();
+     } else if (temperature === "warm" && passport === "no") {
+       $("#jaco").hide();
+       $("#neworleans").show();
+       $("#london").hide();
+       $("#parkcity").hide();
+     } else  if (temperature === "cool" && passport ==="yes"){
+       $("#jaco").hide();
+       $("#neworleans").hide();
+       $("#london").show();
+       $("#parkcity").hide();
+     } else  if (temperature === "cool" && passport ==="no"){
+       $("#jaco").hide();
+       $("#neworleans").hide();
+       $("#london").hide();
+       $("#parkcity").show();
      } else {
-       london += 1;
      }
 
    });
