7a818ef232375b3ed22f1db88ff257aeff60aec6
Susha Dore <susha.dore@gmail.com>
2016-09-23 18:09:25 -0700

    add columns to questions and photos to results

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,27 @@ $(document).ready(function() {
       $("#neworleans").hide();
       $("#london").hide();
       $("#parkcity").show();
-   }  else if (temperature === "warm" && passport === "yes") {
+    } else if (temperature === "warm" && passport === "yes") {
       $("#cabo").show();
       $("#neworleans").hide();
       $("#london").hide();
       $("#parkcity").hide();
-     } else if (temperature === "warm" && passport === "no") {
+    } else if (temperature === "warm" && passport === "no") {
        $("#cabo").hide();
        $("#neworleans").show();
        $("#london").hide();
        $("#parkcity").hide();
-     } else  if (temperature === "cool" && passport ==="yes"){
+    } else  if (temperature === "cool" && passport ==="yes"){
        $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").show();
        $("#parkcity").hide();
-
-     } else  if (temperature === "cool" && passport === "no"){
+    } else  if (temperature === "cool" && passport === "no"){
        $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").hide();
        $("#parkcity").show();
-     } else {
-     }
-
-   });
+    } else {
+    }
+  });
 });
