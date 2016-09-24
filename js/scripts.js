b6e4283abcac32fd4ffd4c77253d739761f381b6
Susha Dore <susha.dore@gmail.com>
2016-09-23 17:07:19 -0700

    make the adventure radio button show park city, never london

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-var jaco=0;
-var london=0;
-var newOrleans=0;
-var parkCity=0;
-
 $(document).ready(function() {
  $("#result button").click(function() {
     var temperature = $("input:radio[name=weather]:checked").val();
     var passport = $("input:radio[name=passport]:checked").val();
-     if (temperature === "warm" && passport === "yes") {
-        $("#jaco").show();
-        $("#neworleans").hide();
-        $("#london").hide();
-        $("#parkcity").hide();
+    var activity = $("input:radio[name=activity]:checked").val();
+
+    if (activity ==="adventure" && temperature === "cool" &&  passport ==="yes") {
+      $("#cabo").hide();
+      $("#neworleans").hide();
+      $("#london").hide();
+      $("#parkcity").show();
+   }  else if (temperature === "warm" && passport === "yes") {
+      $("#cabo").show();
+      $("#neworleans").hide();
+      $("#london").hide();
+      $("#parkcity").hide();
      } else if (temperature === "warm" && passport === "no") {
-       $("#jaco").hide();
+       $("#cabo").hide();
        $("#neworleans").show();
        $("#london").hide();
        $("#parkcity").hide();
      } else  if (temperature === "cool" && passport ==="yes"){
-       $("#jaco").hide();
+       $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").show();
        $("#parkcity").hide();
-     } else  if (temperature === "cool" && passport ==="no"){
-       $("#jaco").hide();
+
+     } else  if (temperature === "cool" && passport === "no"){
+       $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").hide();
        $("#parkcity").show();
