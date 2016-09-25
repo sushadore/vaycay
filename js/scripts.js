15b3a3873e56e1323ffd7dac5dcb6457c2f2ea9b
Susha Dore <susha.dore@gmail.com>
2016-09-25 13:12:38 -0700

    add functionality to return to survey button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
- $("#result button").click(function() {
-   $(".survey-hidden").hide();
+ $("button#show").click(function() {
     var temperature = $("input:radio[name=weather]:checked").val();
     var passport = $("input:radio[name=passport]:checked").val();
     var activity = $("input:radio[name=activity]:checked").val();
@@ -10,28 +9,46 @@ $(document).ready(function() {
       $("#neworleans").hide();
       $("#london").hide();
       $("#parkcity").show();
+      $("#survey-hidden").hide();
+      $("button#survey").show();
     } else if (temperature === "warm" && passport === "yes") {
       $("#cabo").show();
       $("#neworleans").hide();
       $("#london").hide();
       $("#parkcity").hide();
+      $("#survey-hidden").hide();
+      $("button#survey").show();
     } else if (temperature === "warm" && passport === "no") {
        $("#cabo").hide();
        $("#neworleans").show();
        $("#london").hide();
        $("#parkcity").hide();
+       $("#survey-hidden").hide();
+       $("button#survey").show();
     } else  if (temperature === "cool" && passport ==="yes"){
        $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").show();
        $("#parkcity").hide();
+       $("#survey-hidden").hide();
+       $("button#survey").show();
     } else  if (temperature === "cool" && passport === "no"){
        $("#cabo").hide();
        $("#neworleans").hide();
        $("#london").hide();
        $("#parkcity").show();
+       $("#survey-hidden").hide();
+       $("button#survey").show();
     } else {
     }
+  });
 
+  $("button#survey").click(function() {
+    $("#survey-hidden").show();
+    $("#cabo").hide();
+    $("#neworleans").hide();
+    $("#london").hide();
+    $("#parkcity").hide();
+    $("button#survey").hide();
   });
 });
